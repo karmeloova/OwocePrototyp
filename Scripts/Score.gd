819919c575e8e0e43e6badedd1253ce8cd6e0243
karmeloova <olga.maruszynska@gmail.com>
2024-03-11 19:07:02 +0100

    Dopasowanie Platformy Do Koloru
    Punkty dostaje się tylko gdy kolor platformy zgodny z kolorem "owocu"

--- Scripts/Score.gd
+++ Scripts/Score.gd
@@ -1,15 +1,27 @@
 extends Node2D
 var points = 0;
-
+var basketColor = 0;
+var fruitColor = 0;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Label.text = "Score: 0" 
 	SignalManager.addPoints.connect(_on_addPoints);
+	SignalManager.basketColor.connect(_on_basketColor);
+	SignalManager.fruitColor.connect(_on_fruitColor);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_addPoints() :
-	points += 1;
-	$Label.text = "Score: " + str(points);
+	if(basketColor==fruitColor) : 
+		points += 1;
+		$Label.text = "Score: " + str(points);
+
+func _on_basketColor(color) :
+	basketColor = color;
+	
+func _on_fruitColor(color) :
+	fruitColor = color;
+	
+
--- Scripts/fruits.gd
+++ Scripts/fruits.gd
@@ -11,7 +11,7 @@ func _ready():
 		1: $FruitSprite.modulate = Color.GREEN;
 		2: $FruitSprite.modulate = Color.BLUE;
 		3: $FruitSprite.modulate = Color.PURPLE;
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -21,8 +21,7 @@ func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index)
 	match area.name :
 		"Basket": what = 0; #Kolizja z koszykiem (graczem)
 		"Floor": what = 1; #Kolizja z podłogą (gracz nie zdążył)
-	
+	SignalManager.fruitColor.emit(color);
 	SignalManager.fruitColl.emit(what);
 	queue_free();
-	#print(area.name)
 
